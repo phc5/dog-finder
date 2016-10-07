9ea5bf90ce5a6e18169f978268bc37104b8e1b77
Paul Chong <paulhyunchong@gmail.com>
2016-10-06 17:21:53 -0700

    move images to left and map to the right, when user submits zipcode the map will go to that zipcode, clicking on marker highlights dogs, and markers will disappear if new query

--- app.js
+++ app.js
@@ -1,35 +1,40 @@
 //GoogleMaps: AIzaSyAxU3qsOBmOTtQyL0l-_Bpj1OA44YkNI3E
-//petfinder key: b46b11a8ca45dd7cc2a391730e286cca
+//petfinder key: b46b11a8ca45dd7cc2a391730e286cca, 64568269689a7b256e759afcc3c0a52b
 // secret: bf32754638b3c39d17dcf25495090279
 
-var PET_BASE_URL = 'http://api.petfinder.com/pet.find?format=json&callback=?&key=b46b11a8ca45dd7cc2a391730e286cca';
+var PET_BASE_URL = 'http://api.petfinder.com/pet.find?format=json&callback=?&key=64568269689a7b256e759afcc3c0a52b';
 var LAT_LONG_URL = 'http://maps.googleapis.com/maps/api/geocode/json';
 var map;
+var marker;
+var markers = [];
 
-function getDataFromPetFinder(dogBreed, zipCode, call) {
+function getDataFromPetFinder(dogBreed, zipCode, callback) {
 	var query = {
 		animal: 'dog',
 		breed: dogBreed,
 		location: zipCode,
 	}
-	//puts above info together with api url to call callback with the data
-	//be able to receive data to load into the callback function
-	$.getJSON(PET_BASE_URL, query, call);
+	$.getJSON(PET_BASE_URL, query, callback);
 }
 
-//function to get long and lat
-function getLongLat(zip, callback, hoverText) {
+function getLongLatInitial(zip, callback) {
 	var query = {
 		address: zip,
 		sensor: false
 	}
-	//puts info together and loads callback with data from api call
+	$.getJSON(LAT_LONG_URL, query, setMap);
+}
+
+function getLongLatMarker(zip, callback, hoverText) {
+	var query = {
+		address: zip,
+		sensor: false
+	}
 	$.getJSON(LAT_LONG_URL, query, function(data) {
-		callback(data, hoverText);
+		callback(data, hoverText,zip);
 	});
 }
 
-//callback function for data
 function displayResults(data) {
 	var resultElement = '';
 	var imageURL = '';
@@ -38,10 +43,8 @@ function displayResults(data) {
 	if (data.petfinder.pets.pet) {
 		data.petfinder.pets.pet.forEach(function(dog) {
 			if (dog.media.photos) {
-				resultElement += '<p><img src="' + dog.media.photos.photo[0].$t + 
-				'"><br>Name: ' + dog.name.$t + '<br>Age: ' + dog.age.$t + '<br>Zip: ' + 
-				dog.contact.zip.$t + '</p>';
-				//linking zipcode to dog names
+				resultElement += '<p class=zip' + dog.contact.zip.$t + '><img src="' + dog.media.photos.photo[0].$t + '"><br>Name: ' + dog.name.$t + '<br>Age: ' + 
+				dog.age.$t + '<br>Zip: ' + dog.contact.zip.$t + '</p>';
 				var zip = dog.contact.zip.$t;
 				if (dogZip[zip]) {
 					dogZip[zip] += ", " + dog.name.$t;
@@ -56,41 +59,56 @@ function displayResults(data) {
 	
 	var unique = Object.keys(dogZip);
 	for(let i = 0; i < unique.length; i++) {
-//accessing each zip code (keys), add markers is the callback function
-//dogZip[unique[i]] is the parameter of callback function, names mapped to zip code
-		getLongLat(unique[i], addMarkers, dogZip[unique[i]]);
+		getLongLatMarker(unique[i], addMarkers, dogZip[unique[i]]);
 	}
 
 	$('.results').html(resultElement)
 }
 
-//callback function to get markers
-function addMarkers(data, hoverText) {
+
+function addMarkers(data, hoverText,zip) {
 	var loc = data.results[0];
-	//way to initialize marker and lat/long
 	var myLatlng = new google.maps.LatLng(loc.geometry.location.lat, loc.geometry.location.lng);
-	var marker = new google.maps.Marker({
+	marker = new google.maps.Marker({
     	map: map,
     	position: myLatlng,
     	title: hoverText
 	});
+	marker.addListener('click', function() {
+		var selector = 'p.zip' + zip;
+		$('p.highlight').removeClass('highlight');
+		$(selector).addClass('highlight');
+	})
+	markers.push(marker);
+}
+
+function setMapOnAll(map) {
+	for(let i = 0; i < markers.length; i++) {
+		markers[i].setMap(map);
+	}
+}
+
+function deleteMarkers(markers) {
+	setMapOnAll(null);
+	markers = [];
+}
+
+function setMap(data) {
+	var loc = data.results[0];
+	map.setCenter(new google.maps.LatLng(loc.geometry.location.lat, loc.geometry.location.lng));
 }
 
 function watchSubmit() {
-	//event listener for submit
 	$('.js-search-form').submit(function(e) {
 		e.preventDefault();
 		var breed = $(this).find('.breed').val();
 		var zipCode = $(this).find('.zip').val();
-		//this is where we use petfinder api
-		//displayResults is the callback function
+		deleteMarkers(markers)
+		getLongLatInitial(zipCode, setMap);
 		getDataFromPetFinder(breed, zipCode, displayResults);
 	});
 }
 
-//1: first to happen
-//when we load the bottom script, calls this map function
-//loads map
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: 33.7073908, lng: -117.7666567},
@@ -98,9 +116,4 @@ function initMap() {
 	});
 }
 
-//2: waiting for submit button, this is where it's called
-//submit triggers our getData function
 $(function(){watchSubmit();});
-
-
-
